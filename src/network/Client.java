72e52a37fe54f1beec3d057d2f5e7fcf99a1ace5
Khalil Haji <khalilalihaji@gmail.com>
2018-02-10 00:02:34 -0500

    Started Server class

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,15 +1,15 @@
 package network;
 
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
   Socket socket;
   InetAddress ip;
 
-  public Client(Socket socket, String ip) {
+  public Client(Socket socket, String ip) throws UnknownHostException {
     this.socket = socket;
-    this.ip = InetAddress.getByName();
+    this.ip = InetAddress.getByName(ip);
   }
 }
---
+++ src/network/Server.java
@@ -0,0 +1,8 @@
+package network;
+
+import java.net.ServerSocket;
+
+public class Server {
+  ServerSocket serverSocket;
+  int port;
+}
